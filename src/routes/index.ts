16eb25ddc43a6c08a1dc1b085edf45969a7bf592
willslaq <willslaqtomaz@gmail.com>
2021-04-18 16:08:37 -0300

    Iniciado separação em models, tabalhado validação de data removendo o timestamps, validando se já há algum agendamento na mesma data

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email
-  };
-
-  return response.json({
-    name: name,
-    email: email,
-  });
-});
-
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
