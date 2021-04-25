6fba024baba5ef21525a45fa1d8a48bb5c66822e
willslaq <willslaqtomaz@gmail.com>
2021-04-25 17:06:15 -0300

    Criado Migration para alterar a coluna provider -> provider_id

--- src/database/migrations/1619370330800-CreateUsers.ts
+++ src/database/migrations/1619370330800-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1619370330800 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
